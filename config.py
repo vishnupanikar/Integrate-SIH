733903b46f0a30e87839d2a9358c478af48592c7
vishnupanikar <vpp27299@gmail.com>
2020-07-29 15:45:52 +0530

    changing MONGO_URL

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ class Config:
       WTF_CSRF_ENABLED = True
       SECRET_KEY = 'you-will-never-guess'
       UPLOAD_FOLDER = 'static/uploads/'
-      MONGO_URI = "mongodb+srv://jalshaktiAdmin:jsadmin@jalshakti-cluster.gznyv.mongodb.net/staticDB?retryWrites=true&w=majority"
+      MONGO_URI = "mongodb+srv://jalshaktiAdmin:jsadmin@jalshakti-cluster.gznyv.mongodb.net/dynamicDB?retryWrites=true&w=majority"
       
